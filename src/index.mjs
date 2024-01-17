bad00bcc6027ea16daa6b20ff9375363f1ce4009
telemacowill <aglwill@gmail.com>
2024-01-17 12:08:23 -0300

    Get Requests

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
@@ -6,6 +6,18 @@ const PORT = process.env.PORT || 3000;
 
 app.get("/", (request, response) => {
     response.status(201).send({ msg: "Hello"});
+});
+
+app.get("/api/users", (request, response) => {
+    response.send([
+        { id: 1, username : "anson", displayName:"Anson" },
+        { id: 2, username : "jack", displayName:"Jack" },
+        { id: 3, username : "adam", displayName:"Adam" },
+    ]);
+});
+
+app.get("/api/products", (request, response) => {
+    response.send([{id: 123, name: "chicken breast", price:12.99 }]);
 });
 
 app.listen(PORT, () => {
