89ac50c6d4b0b76feba86df86092c031829f8eb6
telemacowill <aglwill@gmail.com>
2024-01-16 21:42:21 -0300

    Get Request

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (request, response) => {
+    response.status(201).send({ msg: "Hello"});
+});
+
 app.listen(PORT, () => {
     console.log('Running on Port ${PORT}');
 }); 
