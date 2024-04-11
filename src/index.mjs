82f503e5398ab782637ef86e200d57075e725a5e
telemacowill <aglwill@gmail.com>
2024-04-11 09:25:38 -0300

    Route Params

--- src/index.mjs
+++ src/index.mjs
@@ -1,19 +1,32 @@
-import express, { response } from 'express';
+import express, { request, response } from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const mockUsers = [
+    { id: 1, username : "anson", displayName:"Anson" },
+    { id: 2, username : "jack", displayName:"Jack" },
+    { id: 3, username : "adam", displayName:"Adam" },
+];
 
 app.get("/", (request, response) => {
     response.status(201).send({ msg: "Hello"});
 });
 
 app.get("/api/users", (request, response) => {
-    response.send([
-        { id: 1, username : "anson", displayName:"Anson" },
-        { id: 2, username : "jack", displayName:"Jack" },
-        { id: 3, username : "adam", displayName:"Adam" },
-    ]);
+    response.send(mockUsers);
+
+});
+
+app.get("/api/users/:id", (request, response) => {
+    console.log(request.params);
+    const parsedId = parseInt(request.params.id);
+    if (isNaN(parsedId)) 
+        return response.status(400).send({ msg: "Bad Request. Invalid ID. " });
+    const findUser = mockUsers.find((user)) => user.id === parsedId);
+    if (!findUser) return response.sendStatus(404);
+    return response.send(findUser);
 });
 
 app.get("/api/products", (request, response) => {
